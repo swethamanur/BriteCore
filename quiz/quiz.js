bf97462616ea5d7640f00bbd273a71f25b8e3011
swetha <swetha.mmanur@gmail.com>
2019-03-23 02:08:51 -0700

    added quiz.js and quiz_1.js

---
+++ quiz/quiz.js
@@ -0,0 +1,10 @@
+let fernet = require('fernet')
+
+let secret = new fernet.Secret('TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM=')
+// Oh no! The code is going over the edge! What are you going to do?
+let message = 'gAAAAABclfNY0cetoKL7XuLGgVnitt0rREeyq6tOOezZ2qmr86pYtMPRKb8A8p4kLAMmb0aQ9LH3Fpd4C9l7g4gVmQZ61qFs-jwhhxqxpR6sLxkIBjcsj7p5RZuI8MQlhro1DcT4ttm9ExRbTNxEY7zYge8tcmpEVeyPPva1yfK8PM2sk9AOqwEVvleqbiH1IqWUzjXdIBr9'
+//giving options to the token object.
+let token = new fernet.Token({secret: secret, token: message, ttl:0})
+token.decode();
+
+//Got null or empty response.
